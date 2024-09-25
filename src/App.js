39cbe4aa089f2c2a99d8d32dd86a6ec364fe55be
Sumanth-M-D <sumanthmdevadiga67@gmail.com>
2024-09-25 11:57:01 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Wallet", quantity: 12, packed: false },
-  { id: 3, description: "Cloths", quantity: 2, packed: false },
-  { id: 4, description: "Charger", quantity: 12, packed: true },
+  { id: 2, description: "Wallet", quantity: 1, packed: false },
+  { id: 3, description: "Cloths", quantity: 4, packed: false },
 ];
 
 function App() {
